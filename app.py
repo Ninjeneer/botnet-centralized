90990e12f51fc1f66da303911af7e4915c56627d
Loan ALOUACHE <alouache.loan@gmail.com>
2021-10-01 14:10:59 +0200

    Switched to websocket

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-from flask import Flask, request, Response
-import os, json
-import socketio
-
+from flask import Flask, request
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-sio = socketio.AsyncServer()
-sockets = []
+sio = SocketIO(app)
+
+
+@sio.on('connect')
+def connect():
+    print("Socket connected")
+
 
 @sio.event
-def connect(sid, environ, auth):
-    sockets.append(sid)
+def disconnect():
+    print("Socket disconnected")
 
-@sio.event
-def disconnect(sid):
-    sockets.remove(sid)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["POST"])
 def command_control():
     print(request.get_json())
-    if sockets:
-        for s in sockets:
-            sio.emit("command", request.get_json(),room=s)
+    emit("command", request.get_json(), namespace="/", broadcast=True)
     return "Done"
 
+
 if __name__ == '__main__':
-    app.run(run="0.0.0.0",debug=true)
+    sio.run(app)
